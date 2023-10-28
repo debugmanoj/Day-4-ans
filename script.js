8de370aa34b7568edb9301f73699c41fab0803f0
debugmanoj <manojkumarwork01@gmail.com>
2023-10-28 20:46:20 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// Do the below programs in anonymous function & IIFE
+
+
+// Q1. Print odd numbers in an array
+
+// odd program using function
+
+var a=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17];
+function odd(values){
+    for(var i=0;i<values.length;i++){
+
+        
+    }
+}
+odd(a)
+
