35068112d49562d2eeece56e6e2a578c3044ae74
debugmanoj <manojkumarwork01@gmail.com>
2023-10-29 18:50:04 +0530

    second commit

--- script.js
+++ script.js
@@ -7,10 +7,81 @@
 
 var a=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17];
 function odd(values){
+    console.log("I am normal function")
     for(var i=0;i<values.length;i++){
-
+        if(values[i]%2!=0){
+            console.log(values[i])
+        }
         
     }
 }
 odd(a)
 
+//odd program using arrow function 
+
+var b=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17];
+var odd=(values)=>{
+    console.log("I am arrow-function")
+    for(var i=0;i<values.length;i++){
+        if(values[i]%2!=0){
+            console.log(values[i])
+        }
+        
+    }
+}
+odd(b)
+
+//odd program using IIFE function
+
+var c=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17];
+(function(values){
+    console.log("I am IFFE function")
+    for(var i=0;i<values.length;i++){
+        if(values[i]%2!=0){
+            console.log(values[i])
+        }
+        
+    }
+})(c)
+
+//Q2.SUm of all numbers in array
+
+//using function 
+
+var d=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17];
+
+function sum(values){
+    var sum=0;
+    console.log("I am sum of array using function")
+    for(var i=0;i<values.length;i++){
+        sum+=values[i];
+    }
+    return sum;
+}
+console.log(sum(d));
+
+//using IIFE function 
+var e=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17];
+(function(values){
+    var sum=0;
+    console.log("I am sum of array using IIFe functions ");
+    for(var i=0;i<values.length;i++){
+        sum+=values[i];
+    }
+    console.log(sum)
+
+})(e)
+
+
+//using arrow function
+var f=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17];
+var a=(values)=>{
+    var sum=0;
+    console.log("I am sum of array using arrow functions ");
+    for(var i=0;i<values.length;i++){
+        sum+=values[i];
+    }
+    return sum;
+
+}
+console.log(a(f));
